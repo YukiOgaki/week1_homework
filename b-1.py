ec755d892cfe9e03dd7feb7b593654b98ff10500
YukiOgaki <teamgift8@gmail.com>
2024-12-02 14:17:35 +0900

    add b-1

---
+++ b-1.py
@@ -0,0 +1,14 @@
+numders = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for numder in numders:
+    print(
+        numder * 1,
+        numder * 2,
+        numder * 3,
+        numder * 4,
+        numder * 5,
+        numder * 6,
+        numder * 7,
+        numder * 8,
+        numder * 9,
+    )
