79c3336242dc9d4f2b4b0d00b281a804c3f002f1
YukiOgaki <teamgift8@gmail.com>
2024-12-03 22:27:25 +0900

    Ieyasu's age was unified to 73

--- customer.py
+++ customer.py
@@ -48,7 +48,7 @@ class Customer:
 
 ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
 tom = Customer(first_name="Tom", family_name="Ford", age=57)
-ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=75)
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
 michelle = Customer(first_name="Michelle", family_name="Tanner", age=3)
 
 # 以降で各問のコードを追加していく
@@ -61,7 +61,7 @@ print("")
 print("C-2. 年齢という概念の追加")
 print(ken.age)  # 15 という値を出力
 print(tom.age)  # 57 という値を出力
-print(ieyasu.age)  # 75 という値を出力
+print(ieyasu.age)  # 73 という値を出力
 
 print("")
 print("C-3. 年齢に応じた適切な入場料(entry_fee)を計算できる")
@@ -73,7 +73,7 @@ print("")
 print("C-4. 単一の顧客情報をCSV形式で取得できる")
 print(ken.info_csv())  # "Ken Tanaka,15,1000" という値を出力
 print(tom.info_csv())  # "Tom Ford,57,1500" という値を出力
-print(ieyasu.info_csv())  # "Ieyasu Tokugawa,75,1200" という値を出力
+print(ieyasu.info_csv())  # "Ieyasu Tokugawa,73,1200" という値を出力
 
 print("")
 print("C-5. 3歳以下の入場料金の無料化")
@@ -85,7 +85,6 @@ print(michelle.entry_fee())  # 0 という値を出力
 print("")
 print("C-6. 75歳以上の料金区分の追加")
 print("     追加済み")
-print("     ※'Ieyasu Tokugawa'の年齢=75、料金=500で固定しています")
 
 print("")
 print("C-7. 単一顧客の情報取得形式の追加その1")
@@ -98,5 +97,5 @@ print("")
 print("C-8. 単一顧客の情報取得形式の追加その2")
 print(ken.info_grid())  # "Ken Tanaka|15|1000" という値を出力
 print(tom.info_grid())  # "Tom Ford|57|1500" という値を出力
-print(ieyasu.info_grid())  # "IIeyasu Tokugawa|75|1200" という値を出力
+print(ieyasu.info_grid())  # "IIeyasu Tokugawa|73|1200" という値を出力
 print(michelle.info_grid())  # "Michelle Tanner|3|0" という値を出力
