4752d8d01c3ceb2baaafa3f2151a82d0cf0ce4dd
YukiOgaki <teamgift8@gmail.com>
2024-12-04 19:24:50 +0900

    ed d-2.py

--- d-2.py
+++ d-2.py
@@ -3,16 +3,16 @@ import math
 
 class Rectangle:
     # コードが期待通り動作するように実装
-    def __init__(self, height, width):
+    def __init__(self, height: float, width: float):
         self.height = height
         self.width = width
 
     def area(self):
-        area_rectangle = float(self.height) * float(self.width)
+        area_rectangle = self.height * self.width
         return f"{area_rectangle:.2f}"
 
     def diagonal(self):
-        diagonal_rectangle = math.sqrt(float(self.height) ** 2 + float(self.width) ** 2)
+        diagonal_rectangle = math.sqrt(self.height**2 + self.width**2)
         return f"{diagonal_rectangle:.2f}"
 
 
